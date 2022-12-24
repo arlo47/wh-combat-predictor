8661b11696258bf5892c9a74035141bf5739f8c5
Greg <gfitzpatrick2@gmail.com>
2022-12-24 12:47:49 -0500

    added rollToHit & utility methods for combat

--- src/app.ts
+++ src/app.ts
@@ -1,3 +1,4 @@
+import { CombatHandler } from './models/combat/CombatHandler';
 import { RollingHandler } from './models/dice/RollingHandler';
 import { Unit } from './models/Unit';
 
@@ -6,4 +7,6 @@ const rollingHandler = RollingHandler.getInstance();
 const attackingUnit = new Unit(3, 5, 4, 5, 2, 7, 6, 10);
 const defendingUnit = new Unit(3, 5, 4, 5, 2, 7, 6, 10);
 
-console.log(rollingHandler.rollDice(3));
+const comabtHandler = CombatHandler.getInstance();
+
+const results = comabtHandler.rollToHit(attackingUnit, defendingUnit);
--- src/models/combat/CombatHandler.ts
+++ src/models/combat/CombatHandler.ts
@@ -1,4 +1,5 @@
 import { RollingHandler } from '../dice/RollingHandler';
+import { Unit } from '../Unit';
 
 export class CombatHandler {
   private static instance: CombatHandler;
@@ -14,5 +15,55 @@ export class CombatHandler {
     }
 
     return CombatHandler.instance;
+  }
+
+  /**
+   * Compares Strength to Toughness and returns the required
+   * result to pass the wound test
+   *
+   * Is S TWICE or more than T        2+
+   * Is S GREATER than T              3+
+   * Is S EQUAL to T                  4+
+   * Is S LOWER than T                5+
+   * Is S HALF or less than T         6+
+   */
+  public static compareStrengthToToughness(s: number, t: number): number {
+    if (s * 2 >= t) {
+      return 2;
+    } else if (s > t) {
+      return 3;
+    } else if (s === t) {
+      return 4;
+    } else if (s / 2 <= t) {
+      return 6;
+    } else if (s < 6) {
+      return 5;
+    } else {
+      throw new Error('Error compaing strength to toughhness');
+    }
+  }
+
+  private filterFailedRoles(
+    diceResults: number[],
+    numberToBeat: number
+  ): number[] {
+    const successfulRolls = diceResults.filter((die) => {
+      return die >= numberToBeat;
+    });
+    return successfulRolls;
+  }
+
+  public rollToHit(attacker: Unit, defender: Unit) {
+    const attackerRoleResults: number[] = this.rollingHandler.rollDice(
+      attacker.models
+    );
+    console.log(attackerRoleResults);
+
+    const successfulHits = this.filterFailedRoles(
+      attackerRoleResults,
+      attacker.ws
+    );
+
+    console.log(successfulHits);
   }
 }
