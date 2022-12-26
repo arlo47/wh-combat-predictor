f648b8edff0f444f4e3d810dde5fcace3c73ca1c
Greg <gfitzpatrick2@gmail.com>
2022-12-26 09:19:27 -0500

    started processCombat method

--- src/models/combat/CombatHandler.ts
+++ src/models/combat/CombatHandler.ts
@@ -27,7 +27,7 @@ export class CombatHandler {
    * Is S LOWER than T                5+
    * Is S HALF or less than T         6+
    */
-  public compareStrengthToToughness(s: number, t: number): number {
+  private compareStrengthToToughness(s: number, t: number): number {
     if (s * 2 >= t) {
       return 2;
     } else if (s > t) {
@@ -57,6 +57,22 @@ export class CombatHandler {
     return successfulRolls;
   }
 
+  /**
+   * Processes all phases of combat:
+   *
+   * Roll To Hit:
+   * Roll one D6; hit scored if result equals or beats attacking model’s BS.
+   * Otherwise the attack fails.
+   *
+   * Roll To Wound:
+   * Roll one D6 and compare the attack's S with the target's T on the table below.
+   * Target wounded if score equals or beats the required result. Otherwise the attack fails.
+   *
+   * Roll To Save:
+   * Roll one D6 and modify it by the attack’s AP. If the result is less than the Sv of the
+   * selected model, the saving throw is failed and it suffers damage. Otherwise the attack
+   * is saved.
+   */
   public processCombat(attacker: Unit, defender: Unit): number {
     /**
      * roll to hit
@@ -65,10 +81,27 @@ export class CombatHandler {
      * (add in morality phase)
      * return damage
      */
+    const successfulHits: DieFaces[] = this.rollToHit(attacker);
+    const successfulWounds: DieFaces[] = this.rollToWound(
+      successfulHits,
+      attacker,
+      defender
+    );
+    const successfulSavingThrows: DieFaces[] = this.rollToSave(
+      successfulWounds,
+      attacker,
+      defender
+    );
+
+    // TODO process wounds with saving throw to calculate damage
+
     return 0;
   }
 
-  public rollToHit(attacker: Unit): DieFaces[] {
+  /**
+   * Processes hit phase of combat.
+   */
+  private rollToHit(attacker: Unit): DieFaces[] {
     const attackerRoleResults: DieFaces[] = this.rollingHandler.rollDice(
       attacker.models
     );
@@ -80,7 +113,10 @@ export class CombatHandler {
     return successfulHits;
   }
 
-  public rollToWound(
+  /**
+   * Processes wound phase of combat.
+   */
+  private rollToWound(
     rolls: DieFaces[],
     attacker: Unit,
     defender: Unit
@@ -94,7 +130,10 @@ export class CombatHandler {
     return successfulWounds;
   }
 
-  public rollSavingThrow(
+  /**
+   * Processes saving throw phase of combat.
+   */
+  private rollToSave(
     rolls: DieFaces[],
     attacker: Unit,
     defender: Unit
